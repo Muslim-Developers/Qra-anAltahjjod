7a65502f163752a87759e8c0d0f879f463ad51ab
bayanradaideh <bayanmradaideh@gmail.com>
2019-12-07 08:49:17 +0200

    Improve UX

---
+++ www/js/index.js
@@ -0,0 +1,15 @@
+
+window.addEventListener('load', () => {
+  registerSW();
+});
+
+
+async function registerSW() {
+  if ('serviceWorker' in navigator) {
+    try {
+      await navigator.serviceWorker.register('./service-worker.js');
+    } catch (e) {
+      console.log(`SW registration failed`);
+    }
+  }
+}
--- www/service-worker.js
+++ www/service-worker.js
@@ -1,11 +1,49 @@
-self.addEventListener('activate', function (event) {
+const cacheName = 'news-v1';
+const staticAssets = [
+  './',
+  'index.html',
+  'templates/3.html',
+  'templates/4.html',
+  'css/style.css',
+  'js/app.js',
+  'js/controllers.js',
+  'js/index.js'
+];
 
+self.addEventListener('install', async e => {
+  const cache = await caches.open(cacheName);
+  await cache.addAll(staticAssets);
+  return self.skipWaiting();
 });
 
-self.addEventListener('fetch', function (event) {
-
+self.addEventListener('activate', e => {
+  self.clients.claim();
 });
 
-self.addEventListener('push', function (event) {
-
+self.addEventListener('fetch', async e => {
+  const req = e.request;
+  const url = new URL(req.url);
+  if (url.origin === location.origin) {
+    e.respondWith(cacheFirst(req));
+  } else {
+    e.respondWith(networkAndCache(req));
+  }
 });
+
+async function cacheFirst(req) {
+  const cache = await caches.open(cacheName);
+  const cached = await cache.match(req);
+  return cached || fetch(req);
+}
+
+async function networkAndCache(req) {
+  const cache = await caches.open(cacheName);
+  try {
+    const fresh = await fetch(req);
+    await cache.put(req, fresh.clone());
+    return fresh;
+  } catch (e) {
+    const cached = await cache.match(req);
+    return cached;
+  }
+}
